e723d7aa5f5c2dc38191757c9f4ba063a8ea88f5
Mônica Picaço <movioloncelo@gmail.com>
2023-02-24 22:25:32 -0300

    Atualizando o mod16

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
+
 function funcaoPadrao(callback) {
-    console.log("Executando via Gulp");
-    callback();
+    setTimeout(function(){
+        console.log("Executando via Gulp");
+        callback();
+    }, 2000);
 }
 
 function dizOi(callback) {
-    console.log("Olá, Gulp");
-    dizTchau();
-    callback();
+    setTimeout(function(){
+        console.log("Olá, Gulp");
+        dizTchau();
+        callback();
+    })
 }
 
 function dizTchau() {
     console.log("Tchau, Gulp");
 }
-exports.default = funcaoPadrao;
+exports.default = gulp.series(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false } , gulp.series(compilaSass));
+
+}
